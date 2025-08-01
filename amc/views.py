9a3d9c8fd260e1d992daeb1f5228627c77192b4c
jana <sakthiveljanakan@gmail.com>
2025-08-01 13:15:48 +0530

    error fix

--- amc/views.py
+++ amc/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .models import AMC, AMCType, PaymentTerms
+from .models import AMC, AMCType, PaymentTerms,Item
 from .serializers import AMCSerializer, AMCTypeSerializer, PaymentTermsSerializer,CustomerSerializer
 import csv
 import io
